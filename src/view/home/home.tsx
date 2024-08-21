9ee86807761e03b74f733e22c980ce375ae90408
adrien <18710735867@163.com>
2024-08-21 22:02:17 +0800

    feat: 重新使用 tailwind css

--- src/view/home/home.tsx
+++ src/view/home/home.tsx
@@ -1,5 +1,3 @@
-import { TechnologyStackPanel } from "./technology-stack-panel";
-
 import HomeBg from "@/assets/images/bg4.png";
 import { Layout } from "@/component/layout";
 
@@ -7,20 +5,20 @@ import { Writing } from "@/component/writing";
 
 export const Home = () => {
   return (
-    <Layout>
-      <div
-        className="h-full bg-repeat bg-center bg-cover"
-        style={{ backgroundImage: `url(${HomeBg})` }}
-      >
-        <div className="absolute -top-2/4 -right-0 flex items-center justify-centers">
-          <div className="text-white font-medium text-[32px]">
-            <Writing texts={["人生是旷野，不是轨道"]} cursorChar={"_"} />
-          </div>
-        </div>
-        {/* <main>
+		<Layout>
+			<div
+				className="h-full bg-repeat bg-center bg-cover"
+				style={{ backgroundImage: `url(${HomeBg})` }}
+			>
+				<div className="w-full absolute top-2/4 right-0">
+					<div className=" text-white font-medium text-[32px] flex items-center justify-center">
+						<Writing texts={["人生是旷野，不是轨道"]} cursorChar={"_"} />
+					</div>
+				</div>
+				{/* <main>
           <TechnologyStackPanel />
         </main> */}
-      </div>
-    </Layout>
-  );
+			</div>
+		</Layout>
+	);
 };
